3da6adfb20240afe278e134be143f69acd590aaf
kenjimaeda54 <kenjimaedafamily@gmail.com>
2021-04-23 20:50:28 -0300

    erros

--- error/error.js
+++ error/error.js
@@ -1,29 +1,33 @@
+
+// new Error e um objeto construtor com ele e possivel tratar erro identico
+//ao erro interno
 function soma(x, y) {
-    if (typeof y !== 'number' || typeof x !== 'number') {
-        throw new Error("X e Y precisam ser mumeros");
+    if (typeof x != 'number' || typeof y != 'number') {
+        throw new Error('x é y precisam ser numeros')
         return;
     }
-
-    return x + y;
-
-}
-
-try {
-
-    const error = soma('1', 2);
-
-} catch (error) {
-
-    console.log("Precisa mostrar isso para usuario,nunca mostrar erro interno ao usuario")
+    return x + y
 
 }
 
+//try  e uma promisse e volta erro caso não der sucesso
+//preferencia não mostrar erros internos aos usuarios
+
 try {
 
-    const error = soma('1', 2);
+    const all = soma('1', 2)
 
 } catch (error) {
+    console.log('Mostrar aos usuarios erros amigaveis,não internos')
 
+}
+
+//aqui estou retornando o erro criado na função soma() com new Error
+try {
+
+    const all = soma('1', 2)
+
+} catch (error) {
     console.log(error)
 
 }
