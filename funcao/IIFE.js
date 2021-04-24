edd66a54baea1a1d34e9de510741e5fbeaa8cb4f
kenjimaeda54 <kenjimaedafamily@gmail.com>
2021-04-24 17:05:28 -0300

    mais funções

---
+++ funcao/IIFE.js
@@ -0,0 +1,29 @@
+//IFFE -> immediately invoked fucntion expression
+//São funções invocadas automaticamente
+//Assim garanto que menos variaveis globais,ja que elas podem afetar
+//nosso condigo por algo externo
+(function () {
+    console.log("Eu sempre sou invocado")
+
+})();
+
+//passando parametros
+(function (name, altura) {
+
+    console.log(name, ` ${altura}0m`)
+
+})("Luiz", 1.80);
+
+//criando outras funções dentro
+(function () {
+
+    function people() {
+        return `Eu sou o Ricardo`
+    }
+    console.log(people());
+
+})();
+
+
+
+
